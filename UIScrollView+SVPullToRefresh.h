06eaa03f9fb49a78f60525589ba4cbc7024f58b1
tongxb <tongxb@mst365.cn>
2014-09-17 14:54:07 +0800

    mst-SVPullToRefresh

---
+++ UIScrollView+SVPullToRefresh.h
@@ -0,0 +1,64 @@
+//
+// UIScrollView+SVPullToRefresh.h
+//
+// Created by Sam Vermette on 23.04.12.
+// Copyright (c) 2012 samvermette.com. All rights reserved.
+//
+// https://github.com/samvermette/SVPullToRefresh
+//
+
+#import <UIKit/UIKit.h>
+#import <AvailabilityMacros.h>
+#import "LLARingSpinnerView.h"
+
+@class SVPullToRefreshView;
+
+@interface UIScrollView (SVPullToRefresh)
+
+- (void)addPullToRefreshWithActionHandler:(void (^)(void))actionHandler;
+- (void)triggerPullToRefresh;
+
+@property (nonatomic, strong, readonly) SVPullToRefreshView *pullToRefreshView;
+@property (nonatomic, assign) BOOL showsPullToRefresh;
+
+@end
+
+
+enum {
+    SVPullToRefreshStateStopped = 0,
+    SVPullToRefreshStateTriggered,
+    SVPullToRefreshStateLoading,
+    SVPullToRefreshStateFinished,
+    SVPullToRefreshStateAll = 10
+};
+
+typedef NSUInteger SVPullToRefreshState;
+
+@interface SVPullToRefreshView : UIView
+
+@property (nonatomic, strong) UIColor *arrowColor;
+@property (nonatomic, strong) UIColor *textColor;
+@property (nonatomic, strong, readonly) UILabel *titleLabel;
+@property (nonatomic, strong, readonly) UILabel *subtitleLabel;
+@property (nonatomic, readwrite) UIActivityIndicatorViewStyle activityIndicatorViewStyle;
+@property (nonatomic, strong) LLARingSpinnerView *llAringSpinnerView;
+
+@property (nonatomic, readonly) SVPullToRefreshState state;
+@property (nonatomic, assign) BOOL finish;
+
+- (void)setTitle:(NSString *)title forState:(SVPullToRefreshState)state;
+- (void)setSubtitle:(NSString *)subtitle forState:(SVPullToRefreshState)state;
+- (void)setCustomView:(UIView *)view forState:(SVPullToRefreshState)state;
+
+- (void)startAnimating;
+- (void)stopAnimating;
+
+// deprecated; use setSubtitle:forState: instead
+@property (nonatomic, strong, readonly) UILabel *dateLabel DEPRECATED_ATTRIBUTE;
+@property (nonatomic, strong) NSDate *lastUpdatedDate DEPRECATED_ATTRIBUTE;
+@property (nonatomic, strong) NSDateFormatter *dateFormatter DEPRECATED_ATTRIBUTE;
+
+// deprecated; use [self.scrollView triggerPullToRefresh] instead
+- (void)triggerRefresh DEPRECATED_ATTRIBUTE;
+
+@end
